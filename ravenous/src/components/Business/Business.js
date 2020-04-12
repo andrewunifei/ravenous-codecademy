52dff21bfd8d88cf500140c831580cfac386ee06
Andrew <andrewunifei@gmail.com>
2020-04-12 20:59:21 -0300

    Some minor changes like indentation

--- ravenous/src/components/Business/Business.js
+++ ravenous/src/components/Business/Business.js
@@ -26,14 +26,14 @@ class Business extends React.Component{
                 <h2>{business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                    <p>{business.address}</p>
-                    <p>{business.city}</p>
-                    <p>{business.state} {business.zipCode}</p>
+                        <p>{business.address}</p>
+                        <p>{business.city}</p>
+                        <p>{business.state} {business.zipCode}</p>
                     </div>
                     <div className="Business-reviews">
-                    <h3>{business.category}</h3>
-                    <h3 className="rating">{business.rating} stars}</h3>
-                    <p>{business.reviewCount} reviews</p>
+                        <h3>{business.category}</h3>
+                        <h3 className="rating">{business.rating} stars}</h3>
+                        <p>{business.reviewCount} reviews</p>
                     </div>
                 </div>
             </div>
